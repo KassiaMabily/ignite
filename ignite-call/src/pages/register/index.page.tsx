ac2801c0f45f6eef9c28c7bfae71a5f0e2f6742f
Kassia Fraga <kassia.fraga@lumaensino.com.br>
2023-09-11 16:31:31 -0300

    Connect to Google Calendar

--- ignite-call/src/pages/register/index.page.tsx
+++ ignite-call/src/pages/register/index.page.tsx
@@ -1,11 +1,12 @@
 import { zodResolver } from '@hookform/resolvers/zod'
 import { Button, Heading, MultiStep, Text, TextInput } from '@ignite-ui/react'
+import { AxiosError } from 'axios'
 import { useRouter } from 'next/router'
 import { ArrowRight } from 'phosphor-react'
 import { useEffect } from 'react'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
-import { api } from '@/lib/axios'
+import { api } from '../../lib/axios'
 
 import { Container, Form, FormError, Header } from './styles'
 
@@ -48,8 +49,15 @@ export default function Register() {
         name: data.name,
         username: data.username,
       })
+
+      await router.push('/register/connect-calendar')
     } catch (err) {
-      console.log(err)
+      if (err instanceof AxiosError && err?.response?.data?.message) {
+        alert(err.response.data.message)
+        return
+      }
+
+      console.error(err)
     }
   }
 
